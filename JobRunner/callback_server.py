649938ebdf43dab1d11199fc9b6bb13bc4e9d187
Sean Xiang <sijiex@lbl.gov>
2023-04-13 14:41:47 -0700

    reset to lowercase

--- JobRunner/callback_server.py
+++ JobRunner/callback_server.py
@@ -19,16 +19,19 @@ def start_callback_server(ip, port, out_queue, in_queue, token, bypass_token):
     timeout = 3600
     max_size_bytes = 100000000000
     conf = {
-        "TOKEN": token,
-        "OUT_Q": out_queue,
-        "IN_Q": in_queue,
-        "BYPASS_TOKEN": bypass_token,
+        "token": token,
+        "out_q": out_queue,
+        "in_q": in_queue,
+        "bypass_token": bypass_token,
         "RESPONSE_TIMEOUT": timeout,
         "REQUEST_TIMEOUT": timeout,
         "KEEP_ALIVE_TIMEOUT": timeout,
         "REQUEST_MAX_SIZE": max_size_bytes,
     }
+
+    print("before update: ", conf)
     app.config.update(conf)
+    print("after update: ", app.config)
     #app.run(host=ip, port=port, debug=False, access_log=False)
     app.run(host=ip, port=port, debug=True, access_log=False)
 
@@ -47,7 +50,7 @@ async def root(request):
 def _check_finished(info=None):
     global prov
     logger.debug(info)
-    in_q = app.config["IN_Q"]
+    in_q = app.config["in_q"]
     try:
         # Flush the queue
         while True:
@@ -63,11 +66,11 @@ def _check_finished(info=None):
 def _check_rpc_token(token):
     print("token checking")
     print(app.config)
-    if token != app.config.get("TOKEN"):
+    if token != app.config.get("token"):
         print("token passed in is: ", token)
-        print("token in app.config is: ", app.config.get("TOKEN"))
+        print("token in app.config is: ", app.config.get("token"))
         print("token is not right")
-        if app.config.get("BYPASS_TOKEN", True):
+        if app.config.get("bypass_token"):
             print("Bypass token")
             pass
         else:
@@ -85,7 +88,7 @@ def _handle_submit(module, method, data, token):
     _check_rpc_token(token)
     job_id = str(uuid.uuid1())
     data["method"] = "%s.%s" % (module, method[1:-7])
-    app.config["OUT_Q"].put(["submit", job_id, data])
+    app.config["out_q"].put(["submit", job_id, data])
     return {"result": [job_id]}
 
 
@@ -129,7 +132,7 @@ async def _process_rpc(data, token):
         _check_rpc_token(token)
         job_id = str(uuid.uuid1())
         data["method"] = "%s.%s" % (module, method)
-        app.config["OUT_Q"].put(["submit", job_id, data])
+        app.config["out_q"].put(["submit", job_id, data])
         try:
             while True:
                 _check_finished(f'synk check for {data["method"]} for {job_id}')
