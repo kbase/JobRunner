88b1e2e8c77c776fe0d7c70703d976cdc0db144c
Sean Xiang <sijiex@lbl.gov>
2023-04-14 13:28:52 -0700

    print out data

--- JobRunner/callback_server.py
+++ JobRunner/callback_server.py
@@ -46,6 +46,7 @@ def start_callback_server(ip, port, out_queue, in_queue, token, bypass_token):
 @app.route("/", methods=["GET", "POST"])
 async def root(request):
     data = request.json
+    print("data is: ", data)
     if request.method == "POST" and data is not None and "method" in data:
         token = request.headers.get("Authorization")
         response = await _process_rpc(data, token)
