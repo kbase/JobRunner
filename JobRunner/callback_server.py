31a8d0c22e9fd7f38837598bcf330a6b1a0344c4
Sean Xiang <sijiex@lbl.gov>
2023-04-18 11:04:55 -0700

    add rountes

--- JobRunner/callback_server.py
+++ JobRunner/callback_server.py
@@ -41,16 +41,19 @@ def start_callback_server(ip, port, out_queue, in_queue, token, bypass_token):
     print("before update: ", conf)
     print("In scs app.ctx: ", app.ctx)
 
-    # app.config["TOKEN"] = token
-    # app.config["OUT_Q"] = out_queue
-    # app.config["IN_Q"] = in_queue
-    # app.config["BYPASS_TOKEN"] = bypass_token
-    # app.config["RESPONSE_TIMEOUT"] = timeout
-    # app.config["REQUEST_TIMEOUT"] = timeout
-    # app.config["KEEP_ALIVE_TIMEOUT"] = timeout
-    # app.config["REQUEST_MAX_SIZE"] = max_size_bytes
+    app.config["TOKEN"] = token
+    app.config["OUT_Q"] = out_queue
+    app.config["IN_Q"] = in_queue
+    app.config["BYPASS_TOKEN"] = bypass_token
+    app.config["RESPONSE_TIMEOUT"] = timeout
+    app.config["REQUEST_TIMEOUT"] = timeout
+    app.config["KEEP_ALIVE_TIMEOUT"] = timeout
+    app.config["REQUEST_MAX_SIZE"] = max_size_bytes
 
     print("after update: ", app.config)
+
+    app.add_route(root, '/', methods=["GET", "POST"])
+
     #app.run(host=ip, port=port, debug=False, access_log=False)
     app.run(host=ip, port=port, debug=True, access_log=False)
 
