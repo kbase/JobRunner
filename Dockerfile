4c5ca1c6482aa0c509df6860cc5942e9c5405193
Shane Canon <scanon@lbl.gov>
2023-04-25 16:22:41 -0700

    Minor updates

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM python:3.9
 
-ADD . /src
+ADD requirements.txt /src/requirements.txt
 
 WORKDIR "/src"
 
 RUN pip install -r requirements.txt
-
+ADD . /src
 
 CMD [ "python3", "-m", "JobRunner.Callback"]
--- run2.sh
+++ run2.sh
@@ -1,9 +1,10 @@
-podman run --name cb -d \
-   --net host \
+podman run --name cb2 -d \
    -e KB_* \
    -e CALLBACK_PORT=9999 \
    -e JOB_DIR \
+   -e IN_CONTAINER=1 \
    -v $JOB_DIR:$JOB_DIR \
+   -p 9999:9999 \
    -v /var/run/user/16907/podman/podman.sock:/run/docker.sock \
    kbase/callback
 
